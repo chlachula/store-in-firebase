3d32b90b4c6b39ee0c038f4f80de8be549caa404
Josef <josefch@gmail.com>
2025-04-24 18:54:24 -0500

    set env vars

---
+++ set-env.sh
@@ -0,0 +1,5 @@
+# source set-env.sh
+#
+FIREBASE_DATABASE_URL="https://you-project-name-default-rtdb.firebaseio.com/" # YOUR_FIREBASE_DATABASE_URL
+FIREBASE_CREDENTIALS_PATH="serviceAccountKey.json"                            # path/to/your/serviceAccountKey.json 
+PORT="8080"                                                                   # Optional: Define a port
